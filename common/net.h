8b6b087bd5a5ce8487903dfd5f67ac8902a68820
MasterSparkInYourFace <stoner9lab@tutanota.com>
2023-08-14 13:57:23 +0300

    sucket

--- common/net.h
+++ common/net.h
@@ -1,36 +1,13 @@
 #pragma once
 #include <stdint.h>
 
-#ifdef linux
-#include <linux/if_link.h>
-#include <linux/netlink.h>
-#include <linux/rtnetlink.h>
-
-typedef struct Netlink_s {
-	struct sockaddr_nl addr;
-	int socket;
-	uint32_t netlink_sequence;
-} Netlink;
-
-#define RTNETLINK_ATTR_BUF_SIZE 4096
-typedef struct RTNL_LinkRequest_s {
-	struct nlmsghdr header;
-	struct ifinfomsg rt_info;
-	uint8_t rt_attr_buf[RTNETLINK_ATTR_BUF_SIZE];
-} RTNL_LinkRequest;
-
-int  rtnetlink_socket(Netlink *c, uint32_t mcast_group);
-void netlink_close(Netlink *c);
-
-int rtnetlink_link_request(Netlink *c, struct nlmsghdr *hdr, uint16_t type);
-#endif // linux
-
 #ifdef unix
-int unix_bind(const char *path);
-int unix_connect(const char *path);
+#define UF_ABSTRACT 1
+#define UF_BIND     2
+int unix_socket(const char *addr, uint8_t flags);
 #endif
 
-int tcp_bind_s(const char *addr, uint16_t port);
-int tcp_bind_u32(uint32_t addr, uint16_t port);
-int tcp_connect_s(const char *addr, uint16_t port);
-int tcp_connect_u32(uint32_t addr, uint16_t port);
+// port is in HOST BYTE ORDER. do not translate it yourself
+#define TCPF_BIND 1
+#define TCPF_IP6  2
+int tcp_socket(const char *addr, uint16_t port, uint8_t flags);
